571fa2830ce57befc84a4b1e5934c99db00b008a
Rob Hurley <rob@thehurleys.net>
2018-03-30 12:46:55 +0100

    Update index.js

--- frontend/endpoints/index.js
+++ frontend/endpoints/index.js
@@ -1,7 +1,7 @@
 (function (){
     'use strict';
-    module.exports.bookings = "http://localhost:4000";
-    module.exports.customers = "http://localhost:5000";
-    module.exports.owners = "http://localhost:6000";
-    module.exports.restaurants = "http://localhost:7000";
+    module.exports.bookings = "http://bookings:4000";
+    module.exports.customers = "http://customers:5000";
+    module.exports.owners = "http://owners:6000";
+    module.exports.restaurants = "http://restaurants:7000";
 }());
