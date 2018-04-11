c0c423b999d8629c92bad73b738c0c4c7149d4d6
rob-hurley <38248647+rob-hurley@users.noreply.github.com>
2018-04-11 17:41:56 +0100

    Updated to use docker and traefik

--- frontend/endpoints/index.js
+++ frontend/endpoints/index.js
@@ -1,7 +1,7 @@
 (function (){
     'use strict';
-    module.exports.bookings = "http://bookings:4000";
-    module.exports.customers = "http://customers:5000";
-    module.exports.owners = "http://owners:6000";
-    module.exports.restaurants = "http://restaurants:7000";
+    module.exports.bookings = "http://bookings.docker.local";
+    module.exports.customers = "http://customers.docker.local";
+    module.exports.owners = "http://owners.docker.local";
+    module.exports.restaurants = "http://restaurants.docker.local";
 }());
