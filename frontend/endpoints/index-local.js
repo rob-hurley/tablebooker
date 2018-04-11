7aad85e7bfdbc7bca5836fabcd0f7b5622a92c47
rob-hurley <38248647+rob-hurley@users.noreply.github.com>
2018-04-11 17:42:37 +0100

    Create index-local.js

---
+++ frontend/endpoints/index-local.js
@@ -0,0 +1,7 @@
+(function (){
+    'use strict';
+    module.exports.bookings = "http://bookings:4000";
+    module.exports.customers = "http://customers:5000";
+    module.exports.owners = "http://owners:6000";
+    module.exports.restaurants = "http://restaurants:7000";
+}());
