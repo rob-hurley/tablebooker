ae1f441d4f1210920bc80379fc91a1fc0e578af4
rob-hurley <38248647+rob-hurley@users.noreply.github.com>
2018-04-12 09:30:36 +0100

    Update index-docker-traefik.js

--- frontend/endpoints/index-docker-traefik.js
+++ frontend/endpoints/index-docker-traefik.js
@@ -1,7 +1,7 @@
 (function (){
     'use strict';
-    module.exports.bookings = "http://bookings.docker.local";
-    module.exports.customers = "http://customers.docker.local";
-    module.exports.owners = "http://owners.docker.local";
-    module.exports.restaurants = "http://restaurants.docker.local";
+    module.exports.bookings = "http://bookings.tablebooker";
+    module.exports.customers = "http://customers.tablebooker";
+    module.exports.owners = "http://owners.tablebooker";
+    module.exports.restaurants = "http://restaurants.tablebooker";
 }());
